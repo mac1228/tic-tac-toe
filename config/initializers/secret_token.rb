9978852c92943c9a7ffb12609ef3d7b41208d98e
Michael Curd <mac1228@gmail.com>
2012-09-19 11:17:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicTacToe::Application.config.secret_token = '088f73992b7bcd3a4bc986670a3a063c780dab62f06d31a38aaf6515122b6644d94ea8a60eca4daf08e9c209bbc51891c6d4e99d66407651dabaf8a2e32924ea'
